bdd1e943292658f6fd33c363e628913ca6f36b35
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2019-07-05 09:35:32 -0500

    basic server set up

---
+++ server/server.js
@@ -0,0 +1,17 @@
+// includes
+const express = require( 'express' );
+const app = express();
+const bodyParser = require( 'body-parser' );
+
+// uses
+app.use( express.static( 'server/public' ) );
+
+// globals
+const port = 5000;
+
+// spin up server
+app.listen( port, ()=>{
+    console.log( 'server up on:', port );
+})
+
+// routes
