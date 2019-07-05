1b1870b49bef3ad68c6a15ecdb2f446bcb1503d0
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2019-07-05 09:41:53 -0500

    added git ingore. basic setup ready

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,10 @@
 $( document ).ready( onReady );
 
+function addItem( e ){
+    e.preventDefault();
+    console.log( 'in addItem' );
+}
+
 function onReady(){
-    console.log( 'JQ' );
+    $( '#addItemButton' ).on( 'click', addItem );
 }
