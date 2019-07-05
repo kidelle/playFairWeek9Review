c49e0a4b95ef33df9d008387072e718553c25165
Danielle Martain <dmartain@gmail.com>
2019-07-05 13:46:18 -0500

    changes for heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,14 +1,44 @@
+// npm install, npm install url
+
 // require 
 const pg = require( 'pg' );
 
 //globals
 const Pool = pg.Pool;
-const config ={
-    database: 'inventory',
-    host: 'localhost',
-    port: 5432,
-    max: 12,
-    idleTimeOutMillis: 30000
+
+// Both loca & Heroku database config
+let config = {};
+
+if ( process.env.DATABASE_URL ) {
+    // Heroku config
+    const params = url.parse (process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
+     // Heroku puts security on our database!
+        config = {
+        user: auth[0],
+         password: auth[1],
+         // Need to get remote server & port
+         host: params.hostname,
+         port: params.port,
+         // Get the database name by splitting the pathname
+         // and taking 2nd part (array item 1)
+         database: params.pathname.split('/')[1],
+         // last two items doon't change
+         max: 12,
+         idleTimeOutMillis: 30000
+}}
+else {
+    // local config - same as earlier this week
+      // local
+        config = {
+          database: 'playfair_inventory',
+          host: 'localhost',
+          port: 5432,
+          max: 12,
+          idleTimeOutMillis: 30000
+      } //end config
+
+   
 } //end config
 const pool = new Pool( config );
 
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use( express.static( 'server/public' ) );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 
 // globals
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // spin up server
 app.listen( port, ()=>{
